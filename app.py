c9b90e57f04f9fe183a229f98a52cd9f753938ef
Tatenda <62408294+Tatenda20@users.noreply.github.com>
2020-03-19 17:41:42 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def hello_world():
+    return '<p>Hello World!</p>'
+
+
+@app.route('/about')
+def about_page():
+    return '<p>This is the about us page</p>'
+
+
+if __name__ == '__main__':
+    app.run()
